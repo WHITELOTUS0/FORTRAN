d5c39265157f76bc20e189d9bf6705c52183d875
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-02 16:45:17 +0300

    script

---
+++ glorry.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+PRICE_PER_APPLE=5
+echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
+
+MyFirstLetters=ABC
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
+
+greeting='Hello        world!'
+echo $greeting" now with spaces: $greeting"
